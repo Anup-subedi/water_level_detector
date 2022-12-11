497893ea7bcb797d87d1920c10262207f7d8549d
Anup Subedi <106690260+Anup033@users.noreply.github.com>
2022-12-11 21:22:25 +0545

    createdfile

---
+++ index.ino
@@ -0,0 +1,40 @@
+const int analogInPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  // declare pin  to be an output:
+ 
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  delay(2);
+  if((sensorValue>=100)&&(sensorValue<=220)){
+    digitalWrite(2,HIGH);
+    delay(100);
+    }
+  else if((sensorValue>=221)&&(sensorValue<=625)){
+   digitalWrite(3,HIGH);
+   delay(100);
+    }  
+  else if((sensorValue>=626)&&(sensorValue<=700)){
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+    }
+  else{
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+    delay(100);
+    }
+}
+
